faf47e8c6db4e71f8b0317c910194cffffc6de1a
Nayab W <42422512+nayabw@users.noreply.github.com>
2018-08-26 08:24:33 -0500

    Add files via upload

--- bot.js
+++ bot.js
@@ -1,22 +1,10 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-const config = require("./config.json")
 let prefix = "poly:";
 var fs = require("fs");
+require('dotenv').config()
+require('http').createServer().listen(3000)
 var lelCountFile = fs.readFileSync("./variables.txt", "utf8");
-
-//Glitch Uptime
-const http = require('http');
-const express = require('express');
-const app = express();
-app.get("/", (request, response) => {
-  console.log(Date.now() + " Ping Received");
-  response.sendStatus(200);
-});
-app.listen(process.env.PORT);
-setInterval(() => {
-  http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
-}, 280000);
 
 
 client.on("ready", () => {
@@ -65,4 +53,4 @@ client.on("message", (message) => {
       
 
 });
-client.login(config.token);
+client.login(process.env.TOKEN);
