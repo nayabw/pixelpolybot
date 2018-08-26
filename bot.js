2e19ad87065c26dc3823952d1a5d3b7a5f748d10
Nayab W <42422512+nayabw@users.noreply.github.com>
2018-08-26 08:25:31 -0500

    Add files via upload

--- bot.js
+++ bot.js
@@ -2,8 +2,6 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 let prefix = "poly:";
 var fs = require("fs");
-require('dotenv').config()
-require('http').createServer().listen(3000)
 var lelCountFile = fs.readFileSync("./variables.txt", "utf8");
 
 
